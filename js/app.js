46cc075db2cf40a5f1c708abe3ab533a73680784
1190100 <1190100@isep.ipp.pt>
2020-08-28 16:11:42 +0100

    final commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 let navLinks = document.querySelectorAll("nav ul li a");
 
 document.addEventListener("scroll", () => {
+    /* scrollValue allow us to know where we are in the page (y chart) */
 
     let scrollValue = window.scrollY + 10;
 
     navLinks.forEach(link => {
+        /* this function estabilishes the position in the section and makes the hover active or inactive in the 4 links */
 
         let section = document.querySelector(link.hash);
         if (section.offsetTop <= scrollValue && section.offsetTop + section.offsetHeight > scrollValue) {
