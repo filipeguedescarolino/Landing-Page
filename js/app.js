fa054833c531e60635a39b58f19fc3f30e15ca10
1190100 <1190100@isep.ipp.pt>
2020-08-30 15:48:26 +0100

    final commit with reviwed code

--- js/app.js
+++ js/app.js
@@ -1,36 +1,77 @@
-let navLinks = document.querySelectorAll("nav ul li a");
 let toggleNavElement = document.querySelector(".toggle-nav");
 let navLeft = document.querySelector(".nav-left");
 let body = document.querySelector("body");
 
-toggleNavElement.addEventListener("click", () => {
-    navLeft.classList.toggle("expanded-nav");
-});
+const sections = document.querySelectorAll("section");
 
-document.addEventListener("scroll", () => {
-    /* scrollValue allow us to know where we are in the page (y chart) */
+const navItemContainer = document.querySelector("#left-nav-item-container");
 
-    let scrollValue = window.scrollY + 10;
+function createNav() {
+    // Create Nav item dynamically
+    let linkHTML = "";
+    sections.forEach((section) => {
+        linkHTML += `
+        <ul class="nav-left-bar">
+            <li class="nav-left-item">
+                <a href="#${section.id}" class="nav-left-link">
+                    <i class="fas ${section.dataset.icon}"></i>
+                    <span class="link-text"> 
+                        ${section.dataset.title}
+                    </span>
+                </a>
+            </li>
+        </ul>
+        `;
+    });
+    // Adding the dynamically created Nav items inside Nav item container div
+    navItemContainer.innerHTML = linkHTML;
+}
 
-    navLinks.forEach(link => {
-        /* this function estabilishes the position in the section and makes the hover active or inactive in the 4 links */
+function init() {
+    // Accessing the NavLinks here instead of at the top because Nav links <a> is created only
+    // after createNav() method is invoked
+    let navLinks = document.querySelectorAll("nav ul li a");
 
-        let section = document.querySelector(link.hash);
-        if (section.offsetTop <= scrollValue && section.offsetTop + section.offsetHeight > scrollValue) {
-            link.classList.add("active");
-            section.classList.add("active-section")
-        } else {
-            link.classList.remove("active");
-            section.classList.remove("active-section")
-        }
+    toggleNavElement.addEventListener("click", () => {
+        navLeft.classList.toggle("expanded-nav");
     });
 
-    body.addEventListener("click", () => {
-        navLeft.classList.remove("expanded-nav");
-    });
+    document.addEventListener("scroll", () => {
+        /* scrollValue allow us to know where we are in the page (y chart) */
 
-    navLeft.addEventListener("click", (event) => {
-        event.stopPropagation();
-    });
+        let scrollValue = window.scrollY + 10;
 
+        navLinks.forEach((link) => {
+            /* this function estabilishes the position in the section and makes the hover active or inactive in the 4 links */
+
+            let section = document.querySelector(link.hash);
+            if (
+                section.offsetTop <= scrollValue &&
+                section.offsetTop + section.offsetHeight > scrollValue
+            ) {
+                link.classList.add("active");
+                section.classList.add("active-section");
+            } else {
+                link.classList.remove("active");
+                section.classList.remove("active-section");
+            }
+        });
+
+        body.addEventListener("click", () => {
+            navLeft.classList.remove("expanded-nav");
+        });
+
+        navLeft.addEventListener("click", (event) => {
+            event.stopPropagation();
+        });
+    });
+}
+
+// After the DOM is fully loaded and parsed
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Building nav dynamically
+    createNav();
+
+    // Start rest of the process
+    init();
 });
